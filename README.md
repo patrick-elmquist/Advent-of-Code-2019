0be332d849a4af36b039c0c18ad6b9a5aee64ba6
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2019-12-11 09:55:10 +0100

    Better looking solution for Day 11

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My solutions for Advent of Code 2019 written in Kotlin
 > https://adventofcode.com/2019
 
 
-Completed: Day#1-10
+Completed: Day#1-11
--- src/Day11.kt
+++ src/Day11.kt
@@ -1,14 +1,69 @@
 import extension.asLongs
 import extension.csv
-import javafx.geometry.Pos
 import util.Day
 import util.IntCode
 
-// Answer #1:
+// Answer #1: 1909
 // Answer #2:
+//   ## #  # #### #### #  # #  # ###  #  #
+//    # #  # #    #    # #  #  # #  # #  #
+//    # #  # ###  ###  ##   #### #  # ####
+//    # #  # #    #    # #  #  # ###  #  #
+// #  # #  # #    #    # #  #  # #    #  #
+//  ##   ##  #    #### #  # #  # #    #  #
 
-private const val BLACK = 0
-private const val WHITE = 1
+
+fun main() {
+    Day(n = 11) {
+        answer {
+            val program = IntCode(lines.first().csv.asLongs())
+            val canvas = Canvas()
+            val start = Position(0, 0)
+            runPaintProgram(program, canvas, start)
+            canvas.paintedPixels
+        }
+        answer {
+            val program = IntCode(lines.first().csv.asLongs())
+            val start = Position(0, 0)
+            val canvas = Canvas().apply { set(start, 1) }
+            runPaintProgram(program, canvas, start)
+
+            val (minX, maxX) = canvas.pixels.keys.map { it.x }.let { (it.min() ?: 0) to (it.max() ?: 0) }
+            val (minY, maxY) = canvas.pixels.keys.map { it.y }.let { (it.min() ?: 0) to (it.max() ?: 0) }
+            val sb = StringBuilder("\n")
+            for (y in minY..maxY) {
+                for (x in minX..maxX) {
+                    sb.append(if (canvas.pixels[Position(x, y)] == 1) '#' else ' ')
+                }
+                sb.append("\n")
+            }
+            sb.toString()
+        }
+    }
+}
+
+private fun runPaintProgram(program: IntCode, canvas: Canvas, start: Position) {
+    var position = start
+    var dir = Direction.UP
+    while (!program.hasTerminated) {
+        program.run(canvas[position].toLong())
+        canvas[position] = program.outputs[0].toInt()
+        dir = dir.nextDir(program.outputs[1].toInt())
+        position = dir.nextPosition(position)
+    }
+}
+
+private class Canvas() {
+    private val _pixels = mutableMapOf<Position, Int>()
+    val pixels: Map<Position, Int> = _pixels
+    val paintedPixels: Int
+        get() = _pixels.keys.count()
+    operator fun get(index: Position): Int = _pixels.getOrPut(index, { 0 })
+    operator fun set(index: Position, value: Int) = _pixels.set(index, value)
+}
+
+private data class Position(val x: Int, val y: Int)
+
 private enum class Direction {
     LEFT, UP, RIGHT, DOWN;
 
@@ -26,83 +81,3 @@ private enum class Direction {
         DOWN -> current.copy(y = current.y + 1)
     }
 }
-
-fun main() {
-    Day(n = 11) {
-        answer {
-            var position = Position(0, 0)
-            var dir = Direction.UP
-            val canvas = Canvas()
-            val program = IntCode(lines.first().csv.asLongs())
-            val paintedPositions = mutableSetOf<Position>()
-            while (!program.hasTerminated) {
-                val color = canvas[position]
-                program.run(color.toLong())
-                val newColor = program.outputs[0].toInt()
-                val turn = program.outputs[1].toInt()
-
-                paintedPositions.add(position)
-                canvas[position] = newColor
-                dir = dir.nextDir(turn)
-                position = dir.nextPosition(position)
-            }
-            paintedPositions.size
-        }
-        answer {
-            var position = Position(0, 0)
-            var dir = Direction.UP
-            val canvas = Canvas().apply { set(position, WHITE) }
-            val program = IntCode(lines.first().csv.asLongs(), debug = false)
-            val paintedPositions = mutableSetOf<Position>()
-            while (!program.hasTerminated) {
-                val color = canvas[position]
-                program.run(color.toLong())
-                val newColor = program.outputs[0].toInt()
-                val turn = program.outputs[1].toInt()
-
-                paintedPositions.add(position)
-                canvas[position] = newColor
-                dir = dir.nextDir(turn)
-                position = dir.nextPosition(position)
-            }
-
-            val xValues = canvas.pixels.keys.map { it.x }
-            val yValues = canvas.pixels.keys.map { it.y }
-            val minX = xValues.min() ?: 0
-            val maxX = xValues.max() ?: 0
-            val width = maxX - minX
-            val minY = yValues.min() ?: 0
-            val maxY = yValues.max() ?: 0
-            val height = maxY - minY
-
-            val xOffset = if (minX < 0) minX * -1 else 0
-            val yOffset = if (minY < 0) minY * -1 else 0
-
-            val pixels = canvas.pixels.map { (position, color) ->
-                position.copy(x = position.x + xOffset, y = position.y + yOffset) to color
-            }.associate { it.first to it.second }
-            println(minY)
-            println(maxY)
-            for (y in 0..height) {
-                for (x in 0..width) {
-                    val color = pixels[Position(x, y)]
-                    print(if (color == WHITE) '#' else ' ')
-                }
-                println()
-            }
-
-            ""
-        }
-    }
-}
-
-
-private class Canvas() {
-    private val _pixels = mutableMapOf<Position, Int>()
-    val pixels: Map<Position, Int> = _pixels
-
-    operator fun get(index: Position): Int = _pixels.getOrPut(index, { BLACK })
-    operator fun set(index: Position, value: Int) = _pixels.set(index, value)
-}
-
-private data class Position(val x: Int, val y: Int)
