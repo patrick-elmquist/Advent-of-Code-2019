d7d8f89fe24013be193b894b867a604cda927da1
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2019-12-14 12:02:52 +0100

    Improved solution for Day 13

--- src/Day13.kt
+++ src/Day13.kt
@@ -18,20 +18,17 @@ fun main() {
         }
         answer {
             val memory = lines.first().csv.asLongs().toMutableList().also { it[0] = 2 }
-            val engine = IntCode(memory, debug = false)
-            val canvas = createCanvas(engine)
-            
-            println(canvas)
-            while (!engine.hasTerminated) {
-                engine.run(calculatePaddleMovement(canvas.findPaddle(), canvas.findBall()))
-                canvas.render(engine)
-            }
-            score
+            val game = IntCode(memory, debug = false).apply { run() }
+            generateGameSequence(game, Canvas(game)).last()
         }
     }
 }
 
-private var score = -1
+private fun generateGameSequence(game: IntCode, canvas: Canvas) = generateSequence(0) { score ->
+    if (game.hasTerminated) return@generateSequence null
+    game.run(calculatePaddleMovement(canvas.paddlePosition, canvas.ballPosition))
+    canvas.render(score)
+}
 
 private fun calculatePaddleMovement(paddle: Point, ball: Point) = when {
     paddle.x < ball.x -> 1L
@@ -39,42 +36,33 @@ private fun calculatePaddleMovement(paddle: Point, ball: Point) = when {
     else -> 0L
 }
 
-private fun createCanvas(engine: IntCode): Canvas {
-    engine.run() // Get the initial state
-
-    val tiles = engine.outputs.chunked(3)
-    val width = (tiles.map { it[0].toInt() }.max() ?: 0) + 1
-    val height = tiles.size / width
-    val canvas = Canvas(width, height)
-
-    canvas.render(engine) // Populate the canvas with the initial state
-    return canvas
-}
-
-private data class Canvas(val width: Int, val height: Int) {
+private data class Canvas(private val game: IntCode) {
     private val spriteMap = mapOf(0 to ' ', 1 to '@', 2 to '#', 3 to '-', 4 to 'o')
-    private val tiles: CharArray = CharArray(width * height) { ' ' }
+    private val tiles: CharArray
+    private val width: Int
 
-    fun set(coord: Pair<Int, Int>, tile: Int) {
-        tiles[coord.second * width + coord.first] = spriteMap[tile] ?: error("No sprite for $tile")
+    val ballPosition: Point get() = find('o')
+    val paddlePosition: Point get() = find('-')
+
+    init {
+        val initialState = game.updates
+        width = (initialState.map { it[0] }.max() ?: 0) + 1
+        val height = initialState.size / width
+        tiles = CharArray(width * height) { ' ' }
+        render(0)
     }
 
-    fun render(engine: IntCode) =
-        engine.outputs
-            .map { it.toInt() }
-            .chunked(3)
-            .fold(score) { score, instr ->
-                if (instr[0] == -1 && instr[1] == 0) {
-                    instr[2]
-                } else {
-                    set(instr[0] to instr[1], instr[2])
-                    score
-                }
-            }.let { score = it }
+    fun render(currentScore: Int) = game.updates
+        .fold(currentScore) { score, instr ->
+            if (instr[0] == -1 && instr[1] == 0) {
+                instr[2]
+            } else {
+                tiles[instr[0] + instr[1] * width] = spriteMap[instr[2]] ?: error("No sprite for ${instr[2]}")
+                score
+            }
+        }
 
-    fun findBall() = find('o')
-    fun findPaddle() = find('-')
-
+    private val IntCode.updates get() = outputs.map { it.toInt() }.chunked(3)
     private fun find(tile: Char) = tiles.indexOf(tile).let { Point(it % width, it / width) }
 
     override fun toString() =
