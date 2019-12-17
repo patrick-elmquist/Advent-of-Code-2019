0e360be753abe4c0477918b38d3cc2fa8ea638a6
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2019-12-17 14:31:34 +0100

    Done with Day 16 solution

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -9,29 +9,28 @@ private val basePattern = listOf(0, 1, 0, -1)
 
 fun main() {
     Day(n = 16) {
-        answer { generateFftSequence(lines.first(), basePattern).take(100).last().take(8) }
-        answer { answer2(lines.first()).take(8).joinToString("") }
+        answer { fftSequence(lines.first(), basePattern).take(100).last().take(8) }
+        answer {
+            val input = lines.first()
+            val tail = input.length * 10000 - input.take(7).toInt()
+            val asIntList = input.map { Character.getNumericValue(it) }
+            val initial = (0 until 10000).flatMap { asIntList }.takeLast(tail).reversed()
+            (0 until 100)
+                .fold(initial) { out, _ -> simplifiedFftSequence(out).last() }
+                .takeLast(8)
+                .reversed()
+                .joinToString("") }
     }
 }
 
-private fun answer2(input: String): List<Int> {
-    val len = input.length * 10000 - input.take(7).toInt()
-    val start = (0 until 10000).joinToString("") { input }.takeLast(len).map { Character.getNumericValue(it) }.reversed()
-    return (0 until 100)
-        .fold(start) { out, _ -> someSequence(out).last() }
-        .reversed()
-}
-
-private fun someSequence(input: List<Int>) =
+private fun simplifiedFftSequence(input: List<Int>) =
     generateSequence(mutableListOf<Int>()) { output ->
         if (output.size == input.size) return@generateSequence null
         else output.apply { add(((output.getOrNull(output.size - 1) ?: 0) + input[output.size]) % 10) }
     }
 
-private fun generateFftSequence(input: String, base: List<Int>) =
-    generateSequence(applyFft(input, base)) {
-        applyFft(it, base)
-    }
+private fun fftSequence(input: String, base: List<Int>) =
+    generateSequence(applyFft(input, base)) { applyFft(it, base) }
 
 private fun applyFft(input: String, base: List<Int>): String {
     return input.indices
@@ -48,6 +47,5 @@ private fun generatePattern(base: List<Int>, n: Int, len: Int): List<Int> {
     val oneCycleLength = base.size * (n + 1) - 1
     val repeats = ceil(len / oneCycleLength.toFloat()).toInt()
     val pattern = base.flatMap { digit -> (0..n).map { digit } }
-    return (0 until repeats).flatMap { pattern }
-        .drop(1)
+    return (0 until repeats).flatMap { pattern }.drop(1)
 }
