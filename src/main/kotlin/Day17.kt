3bd4a5ace6cc94765c16bbc2d53eda4a43fb99ef
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2019-12-17 21:20:07 +0100

    Solution for Day 17 Part 2

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -5,18 +5,28 @@ import util.IntCode
 import util.Point
 
 // Answer #1: 6212
-// Answer #2:
+// Answer #2: 1016741
 
 private val scaffold = listOf('#', '^', 'V', '<', '>')
 
 fun main() {
     Day(n = 17) {
         answer {
-            val program = IntCode(lines.first().csv.asLongs(), debug = false).apply { run() }
+            val program = IntCode(lines.first().csv.asLongs()).apply { run() }
             val tiles = program.outputs.map { it.toChar() }.joinToString("")
             sumAlignmentParameters(tiles)
         }
-        answer { "Not implemented" }
+        answer {
+            val enableInput = lines.first().csv.asLongs().toMutableList().apply { set(0, 2) }
+            IntCode(enableInput).apply {
+                run()
+                run(*"A,A,B,C,B,C,B,C,C,A\n".toLongInput())
+                run(*"L,10,R,8,R,8\n".toLongInput())
+                run(*"L,10,L,12,R,8,R,10\n".toLongInput())
+                run(*"R,10,L,12,R,10\n".toLongInput())
+                run(*"n\n".toLongInput())
+            }.output
+        }
     }
 }
 
@@ -44,3 +54,5 @@ private val Point.neighbours get() = listOf(
     copy(y = y - 1),
     copy(y = y + 1)
 )
+
+private fun String.toLongInput() = map { it.toLong() }.toLongArray()
