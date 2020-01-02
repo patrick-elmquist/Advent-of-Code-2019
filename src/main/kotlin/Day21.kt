08c1755c55ee37bef497e1ef62ed2bb612c051a0
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2020-01-02 20:35:39 +0100

    Added input for Day 21

---
+++ src/main/kotlin/Day21.kt
@@ -0,0 +1,15 @@
+import extension.asLongs
+import extension.csv
+import util.Day
+import util.IntCode
+import util.Point
+
+// Answer #1:
+// Answer #2:
+
+fun main() {
+    Day(n = 21) {
+        answer { "" }
+        answer { "" }
+    }
+}
