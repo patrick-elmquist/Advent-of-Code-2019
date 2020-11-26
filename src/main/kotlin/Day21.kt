7f2f92d30047d3ef668a3fa2e90772ba38d2f14e
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2020-11-26 21:56:25 +0100

    Added solution for Day 21

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -2,14 +2,42 @@ import extension.asLongs
 import extension.csv
 import util.Day
 import util.IntCode
-import util.Point
 
-// Answer #1:
-// Answer #2:
+// Answer #1: 19357180
+// Answer #2: 1139793906
 
 fun main() {
     Day(n = 21) {
-        answer { "" }
-        answer { "" }
+        answer {
+            val program = lines.first().csv.asLongs()
+            val instructions =
+                    """
+                        OR A T
+                        AND B T
+                        AND C T
+                        NOT T J
+                        AND D J
+                        WALK
+                    """.trimIndent().map(Char::toLong).toLongArray() + 10
+            IntCode(program).apply { run(*instructions) }.output
+        }
+        answer {
+            val program = lines.first().csv.asLongs()
+            val instructions =
+                    """
+                        OR A T
+                        AND B T
+                        AND C T
+                        NOT T J
+                        AND D J
+                        NOT J T
+                        OR E T
+                        OR H T
+                        AND T J
+                        RUN
+                    """.trimIndent().map(Char::toLong).toLongArray() + 10
+            IntCode(program).apply { run(*instructions) }.output
+        }
     }
 }
+
---
+++ src/main/kotlin/Day22.kt
@@ -0,0 +1,14 @@
+import util.Day
+
+// Answer #1:
+// Answer #2:
+
+fun main() {
+    Day(n = 22) {
+        answer {
+        }
+        answer {
+        }
+    }
+}
+
