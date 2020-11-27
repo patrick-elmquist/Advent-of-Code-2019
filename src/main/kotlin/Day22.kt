a20de683ca6d9c46ea01c71a711d3301937dd18c
Patrick Elmquist <patrick.elmquist@sony.com>
2020-11-27 11:38:09 +0100

    Solution for Day 22 Part 2

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -1,9 +1,10 @@
 import util.Day
-import java.lang.IllegalArgumentException
+import java.math.BigInteger
+import java.math.BigInteger.*
 import kotlin.math.absoluteValue
 
 // Answer #1: 7665
-// Answer #2:
+// Answer #2: 41653717360577
 
 fun main() {
     Day(n = 22) {
@@ -11,9 +12,35 @@ fun main() {
             shuffleCardsNaive(10007, lines).indexOf(2019)
         }
         answer {
-
+            findValue(lines)
         }
     }
+}
+
+private fun findValue(instructions: List<String>): BigInteger {
+    val nbrCards = 119315717514047.toBigInteger()
+    val shuffles = 101741582076661.toBigInteger()
+    val find = 2020.toBigInteger()
+
+    val mem = arrayOf(ONE, ZERO)
+    instructions.reversed().forEach { instruction ->
+        when {
+            "cut" in instruction -> mem[1] += instruction.getNumber().toBigInteger()
+            "increment" in instruction -> {
+                instruction.getNumber().toBigInteger().modPow(nbrCards - TWO, nbrCards).also {
+                    mem[0] *= it
+                    mem[1] *= it
+                }
+            }
+            "stack" in instruction -> {
+                mem[0] = mem[0].negate()
+                mem[1] = (mem[1].inc()).negate()
+            }
+        }
+    }
+    val power = mem[0].modPow(shuffles, nbrCards)
+    return ((power * find) + ((mem[1] * (power + nbrCards.dec())) * ((mem[0].dec()).modPow(nbrCards - TWO, nbrCards))))
+            .mod(nbrCards)
 }
 
 private fun shuffleCardsNaive(cardsInDeck: Int, instructions: List<String>): List<Int> {
@@ -40,4 +67,3 @@ private fun List<Int>.deal(increment: Int): List<Int> {
 }
 
 private fun String.getNumber() = this.split(" ").last().toInt()
-
