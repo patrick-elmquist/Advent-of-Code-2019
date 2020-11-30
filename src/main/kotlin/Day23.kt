51e6e9e5d0a740df4c6b3641b7ad95c5404193a0
Patrick Elmquist <patrick.elmquist@sony.com>
2020-11-30 10:23:28 +0100

    Solution for Day 23 Part 2

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -2,33 +2,60 @@ import extension.asLongs
 import extension.csv
 import util.Day
 import util.IntCode
-import java.lang.IllegalStateException
 
 // Answer #1: 23057
-// Answer #2:
+// Answer #2: 15156
+
+private const val NAT_ADDRESS = 255L
+private val NIC_ADDRESSES = 0L until 50L
 
 fun main() {
     Day(n = 23) {
         answer {
             val program = lines.first().csv.asLongs()
-            val nics = (0L until 50L).map { it to IntCode(program, debug = false).apply { run(it) } }.toMap()
-
-            while (true) {
-                val packets = nics.values.flatMap { it.outputs.chunked(3) }
-
-                val deliveredAddresses = packets.map { (address, x, y) ->
-                    if (address == 255L) return@answer "$y"
-                    (nics[address] ?: error("wtf?")).run(x, y)
-                    address
-                }.toSet()
-
-                nics.filter { it.key !in deliveredAddresses }
-                        .forEach { (_, nic) -> nic.run(-1) }
-            }
-
-            ""
+            rebuildNetwork(program, breakOnNat = true)
         }
         answer {
+            val program = lines.first().csv.asLongs()
+            rebuildNetwork(program, breakOnNat = false)
         }
     }
 }
+
+private fun rebuildNetwork(program: List<Long>, breakOnNat: Boolean): Long {
+    val nics = NIC_ADDRESSES.map { address -> address to IntCode(program) }
+            .onEach { (address, nic) -> nic.run(address) }
+            .toMap()
+
+    var natMemory: Packet? = null
+    var lastNatY: Long? = null
+    while (true) {
+        val packets = nics.values.flatMap { nic -> nic.outputs.toPackets() }
+
+        val deliveredAddresses = packets.map { (address, x, y) ->
+            when {
+                address == NAT_ADDRESS && breakOnNat -> return y
+                address == NAT_ADDRESS -> natMemory = Packet(0, x, y)
+                else -> address.also { nics[address]?.run(x, y) }
+            }
+        }
+
+        nics.filterKeys { it !in deliveredAddresses }
+                .forEach { (_, nic) -> nic.run(-1) }
+
+        val isIdle = packets.filter { it.address != NAT_ADDRESS }.none()
+        val natPacket = natMemory
+        if (isIdle && natPacket != null) {
+            if (lastNatY == natPacket.y) return natPacket.y
+            nics[natPacket.address]?.run(natPacket.x, natPacket.y)
+            lastNatY = natPacket.y
+            natMemory = null
+        }
+    }
+}
+
+data class Packet(val address: Long, val x: Long, val y: Long) {
+    constructor(values: List<Long>) : this(values[0], values[1], values[2])
+}
+
+private fun List<Long>.toPackets() = chunked(3).map { Packet(it) }
